cf95fbbbb69c6244e5446d9ad8486f1032948a52
Leonardo Drummond Costa Rodrigues <leonardo.dcr@gmail.com>
2021-01-27 17:49:17 -0300

    Fixes orientation (portrait).

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sheet_music_fluency_practice/pages/practice.dart';
 import 'package:sheet_music_fluency_practice/pages/Home.dart';
 
-void main() => runApp(MaterialApp(
-  initialRoute: '/',
-  routes: {
-    '/': (context) => Home(),
-    '/practice': (context) => Practice(),
-  }
-));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+    .then((value) => runApp(MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+        '/practice': (context) => Practice(),
+      }
+  )));
+}
 
 //https://pub.dev/packages/localstorage
